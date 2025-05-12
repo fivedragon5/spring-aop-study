e0d540055de18301aeb215bdc9dc0b051a72979e
fad <ojy9406@naver.com>
2025-05-12 19:03:54 +0900

    add. AOP Example code

---
+++ src/test/java/spring/aop/AopTest.java
@@ -0,0 +1,38 @@
+package spring.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+ import spring.aop.order.OrderRepository;
+import spring.aop.order.OrderService;
+
+@SpringBootTest
+@Slf4j
+public class AopTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService = {}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository = {}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
