2fce26feb71a4eb10682b18983f58566ef437196
fad <ojy9406@naver.com>
2025-05-17 17:34:04 +0900

    feat: Spring AOP Issue - Proxy Solution

--- src/test/java/spring/aop/proxyvs/ProxyDITest.java
+++ src/test/java/spring/aop/proxyvs/ProxyDITest.java
@@ -12,15 +12,19 @@ import spring.aop.proxyvs.code.ProxyDIAspect;
 /**
  * 프록시 DI 테스트
  * 1. JDK 동적 프록시
- *  - 인터페이스를 기반으로 프록시를 생성하기 때문에 인터페이스로 주입받아야 한다.
+ *  - 인터페이스를 기반으로 프록시를 생성하기때문에 ~~~Impl 같 구체 타입으로 캐스팅이 불가능 하다
+ *
  * 2. CGLIB 동적 프록시
  *  - 클래스 기반으로 프록시를 생성하기 때문에 구현 클래스로 주입받아도 된다.
  *
  * JDK 동적 프록시는 MemberService 인터페이스를 기반으로 프록시를 생성하기 때문에 의존관게 주입이 불가능하다.
+ *
+ * Spring AOP - Proxy 생성시 기본적으로 CGLIB 동적 프록시를 사용한다.
  */
 @Slf4j
 //@SpringBootTest(properties = "spring.aop.proxy-target-class=false") // JDK 동적 프록시
-@SpringBootTest(properties = "spring.aop.proxy-target-class=true") // CGLIB 동적 프록시
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=true") // CGLIB 동적 프록시
+@SpringBootTest
 @Import(ProxyDIAspect.class)
 public class ProxyDITest {
 
